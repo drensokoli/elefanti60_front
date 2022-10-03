cc4bd8d636bd0cacbdaa460fcd56a61bc0b73dcf
drensokoli <79976300+drensokoli@users.noreply.github.com>
2022-10-03 19:55:22 +0200

    Created frontend repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import axios, { AxiosResponse } from 'axios';
+import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  // useEffect(() => {
+  //   fetch(`https://localhost:44391/api/Product/1`)
+  //    .then((response) => console.log(response.data));
+  //  }, []);
+
+  
+  useEffect(() => {
+    axios.get('https://localhost:44391/api/Product/1')
+    .then((response: AxiosResponse<any>) => {
+      console.log(response.data);
+    })
+   }, []);
+
+
+   return(
+    <>
+     test
+     test2
+     test3
+    </>
+   )
 }
 
 export default App;
