634a15de483d7f793323dd3585a36fa7fcd0cb2a
ermiraveliu <114769521+ermiraveliu@users.noreply.github.com>
2022-10-05 12:47:50 +0200

    Create Layout

---
+++ elefanti60_front/components/utils/data.js
@@ -0,0 +1,32 @@
+const data = {
+    products:[
+        {
+            title: 'Telefon',
+            slug: 'telefon',
+            description: 'sdncweioncwpiec',
+            category: 'Telefon',
+            stock: '2',
+            price:'3',
+            image:'images/telefon1.jpg',
+        },
+        {
+            title: 'Telefon',
+            slug: 'telefon',
+            description: 'sdncweioncwpiec',
+            category: 'Telefon',
+            stock: '2',
+            price:'3',
+            image:'images/telefon1.jpg',
+        },      
+        {
+            title: 'Telefon',
+            slug: 'telefon',
+            description: 'sdncweioncwpiec',
+            category: 'Telefon',
+            stock: '2',
+            price:'3',
+            image:'images/telefon1.jpg',
+        },
+        
+    ]
+}
