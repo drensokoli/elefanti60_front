fe1062b21e53c0ebb62009ff90917d8e03d66b29
sopjanielona <106884766+sopjanielona@users.noreply.github.com>
2022-10-05 23:53:19 +0200

    Created an initial state for cart and a reducer function

---
+++ elefanti60_front/utils/Store.js
@@ -0,0 +1,35 @@
+import { createContext, useReducer } from "react";
+
+
+export const Store = createContext();
+
+
+// Posht kemi kriju nje gjendje fillestare te Cart qe eshte nje list e thate
+
+  const initialState = {
+    cart: { cartItems: [] },
+  };
+
+//   Posht kemi kriju nje funksion reducer qe kthen nje gjendje te re, 
+//   duke performu nje aksion ne gjendjen fillestare
+
+  function reducer( state, action) {
+    switch (action.type) {
+        case 'CART_ADD_ITEM': {
+            const newItem = action.payload;
+            const existItem = state.cart.cartItems.find(
+                (item) => item.slug === newItem.slug
+            );
+            const cartItems = existItem? state.cart.cartItems.map((item) => 
+            item.name === existItem.name ? newItem : item
+            )
+            : [...state.cart.cartItems, newItem];
+            return { ...state, cart: {...state.cart, cartItems }}
+            
+        }
+        default:
+            return state;
+    }
+  }
+
+  
