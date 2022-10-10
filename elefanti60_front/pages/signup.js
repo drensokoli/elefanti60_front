034c372dc019f92bf7b558116740fac009e667e4
ermiraveliu <114769521+ermiraveliu@users.noreply.github.com>
2022-10-10 16:47:24 +0200

    Handle sign up using fetch request

--- elefanti60_front/pages/signup.js
+++ elefanti60_front/pages/signup.js
@@ -1,5 +1,14 @@
 import React, { useRef } from 'react'
 import Layout from '../components/Layout';
+import { useState } from 'react';
+
+// export async function getServerSideProps(){
+//   return{
+//     props: {
+
+//     }
+//   }
+// }
 
 export default function SignUpScreen() {
   const name = useRef()
@@ -9,6 +18,7 @@ export default function SignUpScreen() {
   const password = useRef()
   const adress = useRef()
   const cardnumber = useRef()
+
 
 
 
@@ -27,40 +37,68 @@ export default function SignUpScreen() {
     alert('Account created sucessfully')
 
  console.log(name,lastname,username,email,password,adress,cardnumber)
-  }
-  
+  }}
 
- }
+  const handleSubmit = async(event) => {
+    event.preventDefault()
+
+    const data = {
+      name: event.target.name.value,
+      lastName: event.target.lastname.value,
+      username: event.target.username.value,
+      password: event.target.password.value,
+      email: event.target.email.value,
+      address: event.target.address.value,
+      cardnumber: event.target.cardnumber.value
+    }
+
+    const jsonData = JSON.stringify(data);
+    const endpoint = 'https://localhost:7277/api/Users';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: jsonData,
+    }
+
+    const response = await fetch(endpoint, options);
+    const result = await response.json()
+    alert(`Is this your full name: ${result.data}`)
+
+  }  
+
 
   return (
     <Layout title="Sign Up" >
-      <div className='text-center'>
-        <div className= 'mt-24 '>
-          <div className='mb-2.5	'>
-            <input className=' h-8 w-1/5 text-lg'placeholder='Name' type='text' ref={name} />
+      <form onSubmit={handleSubmit}>
+        <div className='text-center'>
+          <div className= 'mt-24 '>
+            <div className='mb-2.5	'>
+              <input className=' h-8 w-1/5 text-lg'placeholder='Name' type='text' ref={name} id="name" name='name'/>
+            </div>
+            <div className='mb-2.5	'>
+              <input className=' h-8 w-1/5 text-lg ' placeholder='Last Name' type='text' ref={lastname} id="lastname" name='lastname' />
+            </div>
+            <div className='mb-2.5'>
+              <input className=' h-8 w-1/5 text-lg ' placeholder='Username' type='text' ref={username} id="username" name='username'/>
+            </div>
+            <div className='mb-2.5 '>
+              <input className=' h-8 w-1/5 text-lg ' placeholder='Email' type='email' ref={email} id="email" name='email'/>
+            </div>
+            <div className='mb-2.5'>
+              <input className=' h-8 w-1/5 text-lg' placeholder='Password' type='password' ref={password} id="password" name='password' />
+            </div>
+            <div className='mb-2.5'>
+              <input className=' h-8 w-1/5 text-lg ' placeholder='Adress' type='text' ref={adress} id="address" name='address' />
+            </div>
+            <div className='mb-2.5'>
+              <input className=' h-8 w-1/5 text-lg ' placeholder='Card Number' type="text" pattern="[0-9]*" ref={cardnumber}  id="cardnumber" name='cardnumber'/>
+            </div>
+            <button className='h-1/3 w-1/5  bg-violet-600 text-white	rounded-md text-md py-1 px-3' onClick={handleClick}>Sign Up</button>
           </div>
-          <div className='mb-2.5	'>
-            <input className=' h-8 w-1/5 text-lg ' placeholder='Last Name' type='text' ref={lastname} />
-          </div>
-          <div className='mb-2.5'>
-            <input className=' h-8 w-1/5 text-lg ' placeholder='Username' type='text' ref={username} />
-          </div>
-          <div className='mb-2.5 '>
-            <input className=' h-8 w-1/5 text-lg ' placeholder='Email' type='email' ref={email} />
-          </div>
-          <div className='mb-2.5'>
-            <input className=' h-8 w-1/5 text-lg' placeholder='Password' type='password' ref={password} />
-          </div>
-          <div className='mb-2.5'>
-            <input className=' h-8 w-1/5 text-lg ' placeholder='Adress' type='text' ref={adress} />
-          </div>
-          <div className='mb-2.5'>
-            <input className=' h-8 w-1/5 text-lg ' placeholder='Card Number' type="text" pattern="[0-9]*" ref={cardnumber} />
-          </div>
-          <button className='h-1/3 w-1/5  bg-violet-600 text-white	rounded-md text-md py-1 px-3' onClick={handleClick}>Sign Up</button>
         </div>
-      </div>
-
+      </form>
     </Layout>
   )
 }
