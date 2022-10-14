7166e0f3701916bbd2f44911a5cf05c698276d39
drensokoli <79976300+drensokoli@users.noreply.github.com>
2022-10-14 20:14:20 +0200

    Update Navbar

--- elefanti60_front/components/Navbar.js
+++ elefanti60_front/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
     const { cart } = state;
 
     return (
-        
+
         <nav className='h-auto items-center px-4 shadow-lg'>
             <div className='flex flex-row h-20 items-center px-4 shadow-lg md:justify-around justify-between'>
                 <div className='gap-10'>
@@ -37,36 +37,37 @@ const Navbar = () => {
                             <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                 <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                             </div>
-                            <input type="search" name="searchTerm" id="default-search" value={searchTerm} onChange={handleInput}class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-30 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black" placeholder="Search for products" required="" />
+                            <input type="search" name="searchTerm" id="default-search" value={searchTerm} onChange={handleInput} class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-30 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black" placeholder="Search for products" required="" />
                             <button type="submit" class="absolute right-2.5 bottom-2.5">{UilSearch}</button>
                         </div>
                     </form>
 
-            </div>
+                </div>
 
-            <div className='flex justify-center items-center gap-10 mr-20'  >
+                <div className='flex justify-center items-center gap-10 mr-20'  >
 
-                <Link href="/cart">
-                    <a className='p-2 '>
-                        <UilShoppingCart/>
-                        {cart.cartItems.length > 0 && (
-                            <span className='ml-1 rounded-full bg-violet-600 px-2 py-1 text-xs font-bold text-white'>
-                                {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                            </span>
-                        )}
-                    </a>
-                </Link>
-
-             
-
-                < DropdownMenu  option={<UilUser/>} className= 'absolute top-14 bottom-72'>
-                 <a className='h-14 flex text-center'>My Profile</a> <br/>
-              <a href='/login' className=' h-14 flex text-center'>My Order History</a>
-              <p className=' h-14 flex text-center'>Log Out</p>
-              {/* <p className='text-center'>Delete Account</p> */}
-            </DropdownMenu>
+                    <Link href="/cart">
+                        <a className='p-2 '>
+                            <UilShoppingCart />
+                            {cart.cartItems.length > 0 && (
+                                <span className='ml-1 rounded-full bg-violet-600 px-2 py-1 text-xs font-bold text-white'>
+                                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                                </span>
+                            )}
+                        </a>
+                    </Link>
 
 
+
+                    < DropdownMenu option={<UilUser />} className='absolute top-14 bottom-72'>
+                        <a className='h-14 flex text-center'>My Profile</a> <br />
+                        <a href='/login' className=' h-14 flex text-center'>My Order History</a>
+                        <p className=' h-14 flex text-center'>Log Out</p>
+                        {/* <p className='text-center'>Delete Account</p> */}
+                    </DropdownMenu>
+
+
+                </div>
             </div>
         </nav>
     );
