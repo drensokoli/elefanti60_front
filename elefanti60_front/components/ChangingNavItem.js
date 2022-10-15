2db0ff40f4a6efc878e8eeb020b2e1e49745325c
sopjanielona <106884766+sopjanielona@users.noreply.github.com>
2022-10-16 01:16:35 +0200

    Sign Out button fix & routing fix

--- elefanti60_front/components/ChangingNavItem.js
+++ elefanti60_front/components/ChangingNavItem.js
@@ -4,6 +4,8 @@ import { CartContexts } from "../contexts/CartContexts";
 import LoginComponent from "./LoginComponent";
 import { UilUser, UilShoppingCart } from '@iconscout/react-unicons';
 import DropdownMenuItem from "./DropdownMenuItem";
+import Router from 'next/router'
+
 
 export default function ChangingNavItem() {
     var id;
@@ -17,25 +19,47 @@ export default function ChangingNavItem() {
         try {
             id = window.localStorage.getItem('id');
             console.log(id, "user");
+            
         } catch (ex) {
             console.log(ex, "error")
         }
         return id;
     }
 
+    const clearLocalStorage = () => {
+        // const id = useContext(CartContexts);
+        // return id;
+
+
+        if (typeof window === "undefined") { console.log("window undefined") }
+        else { console.log("window defined") }
+        try {
+            window.localStorage.clear(getId(id));
+            Router.push('/')
+
+            console.log(id, "user");
+            
+        } catch (ex) {
+            console.log(ex, "error")
+        }
+        return
+        ;
+    }
+
     var userEmpty = getId(id) == null;
     console.log(id, "idd")
     return (
+        <>
         <div>
             {
                 userEmpty ?
                     (<LoginComponent />) :
                     (<DropdownMenuItem option={<UilUser/>} className=' hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600'>
-                        <div className=' shadow-2xl	 bg-white'>
+                        <div className='z-10 shadow-2xl	 bg-white relative right-11'>
                             <div className="divide-y divide-slate-200  py-1 text-sm text-gray-700 dark:text-gray-400 text-center" aria-labelledby="dropdownNavbarButton">
                                 <a href='/userinfo' className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">My Profile</a>
                                 <a href='/userinfo' className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">My Order History</a>
-                                <p className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Log Out</p>
+                                <p className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onClick={clearLocalStorage}>Log Out</p>
                             </div>
                         </div>
                     </DropdownMenuItem>)
@@ -43,6 +67,7 @@ export default function ChangingNavItem() {
 
             }
         </div >
+   </>
     );
 }
 
--- elefanti60_front/pages/login.js
+++ elefanti60_front/pages/login.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
 import Layout from '../components/Layout';
 import Image from 'next/image';
 import logo from '../assets/logo.png'
@@ -11,16 +11,16 @@ export default function LoginScreen() {
   const password = useRef()
   const Router = useRouter()
 
-  const handleClick = () => {
-    if (username.current.value && password.current.value) {
-      localStorage.setItem('username', username.current.value)
-      // localStorage.setItem('email',email.current.value)
-      localStorage.setItem('password', password.current.value)
-      //localStorage.setItem('signUp',email.current.value)
+  // const handleClick = () => {
+  //   if (username.current.value && password.current.value) {
+  //     localStorage.setItem('username', username.current.value)
+  //     // localStorage.setItem('email',email.current.value)
+  //     localStorage.setItem('password', password.current.value)
+  //     //localStorage.setItem('signUp',email.current.value)
 
-      console.log(username, password)
-    }
-  }
+  //     console.log(username, password)
+  //   }
+  // }
 
   const handleSubmit = async (event) => {
     event.preventDefault()
@@ -52,6 +52,7 @@ export default function LoginScreen() {
       const result = await response.json();
       console.log(result);
       localStorage.setItem('id',result)
+
       Router.push('/')
     }
     catch(ex)
@@ -63,6 +64,9 @@ export default function LoginScreen() {
   }
 
 
+  if (!user) return null
+  else 
+
   return (
     <Layout title="Login">
       <div className=' h-screen'>
@@ -73,11 +77,12 @@ export default function LoginScreen() {
                 <Image src={logo} />
               </div>
               <div>
-                <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Username" required="" />
+                <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Adresa juaj elektronike</label>
+                <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Emri i përdoruesit
+" required="" />
               </div>
               <div>
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Fjalekalimi</label>
                 <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required="" />
               </div>
               <div class="flex items-start">
@@ -85,33 +90,21 @@ export default function LoginScreen() {
                   <div class="flex items-center h-5">
                     <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="" />
                   </div>
-                  <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                  <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Me mbaj mend</label>
                 </div>
               </div>
-              <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+              <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Kycu ne llogarine tuaj</button>
               <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                Not registered?
+                Nuk jeni te regjistruar? 
                 <Link href="/signup">
-                  <a href="#" class="text-blue-700 hover:underline dark:text-blue-500"> Create account</a>
+                  <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Krijo nje llogari</a>
                 </Link>
               </div>
             </form>
           </div>
 
         </div>
-        {/* <form onSubmit={handleSubmit}>
-          <div className='text-center'>
-            <div className= 'mt-24 '>
-              <div className='mb-2.5	'>
-                <input placeholder='Username' type='text' ref={username} id="username" name='username' />
-              </div>
-              <div className='mb-2.5'>
-                <input  placeholder='Password' type='password' ref={password} id="password" name='password'/>
-              </div>
-              <button className='h-1/3 w-1/5  bg-violet-600 text-white	rounded-md text-md py-1 px-3' onClick={handleClick}>Log in</button>
-            </div>
-          </div>
-       </form> */}
+      
       </div>
     </Layout>
   )
@@ -142,3 +135,17 @@ export default function LoginScreen() {
 // className=' h-8 w-1/5 text-lg text-center'
 // className='h-8 w-1/5	text-lg text-center'
 // className='h-8 w-1/5	text-lg text-center'
+
+  {/* <form onSubmit={handleSubmit}>
+          <div className='text-center'>
+            <div className= 'mt-24 '>
+              <div className='mb-2.5	'>
+                <input placeholder='Username' type='text' ref={username} id="username" name='username' />
+              </div>
+              <div className='mb-2.5'>
+                <input  placeholder='Password' type='password' ref={password} id="password" name='password'/>
+              </div>
+              <button className='h-1/3 w-1/5  bg-violet-600 text-white	rounded-md text-md py-1 px-3' onClick={handleClick}>Log in</button>
+            </div>
+          </div>
+       </form> */}
