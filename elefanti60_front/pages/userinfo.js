75dcecd431f750c883ea55311ac18a0736dca0cf
drensokoli <79976300+drensokoli@users.noreply.github.com>
2022-10-15 14:22:02 +0200

    User info page updae

--- elefanti60_front/pages/userinfo.js
+++ elefanti60_front/pages/userinfo.js
@@ -9,7 +9,7 @@ export default function UserInfoScreen() {
     return (
         <Layout title="User Info">
             <div className='flex flex-col justify-center items-center gap-4 h-screen mb-5'>
-                <div className=" flex flex-col w-fitp-10 gap-3 h-fit">
+                <div className=" flex flex-col w-fit gap-3 h-fit">
                     <div className=" flex flex-col w-fit border-2 p-10 gap-3 h-fit">
                         <div className='flex flex-row justify-between'>
                             <h1 className='text-3xl text-left text-blue-500 '>User info</h1>
@@ -17,32 +17,29 @@ export default function UserInfoScreen() {
 
                         <div>
                             <h1 className=''>Name:</h1>
-                            <input className='h-9 sm:w-[500px] w-[250px] border-2 rounded' type="text" name="" id="" />
+                            <h5 className='w-[200px] sm:w-[500px]'>Text</h5>
                         </div>
                         <div>
                             <h1 className=''>Last name:</h1>
-                            <input className='h-9 sm:w-[500px] w-[250px] border-2 rounded' type="text" name="" id="" />
+                            <h5 className='w-[200px] sm:w-[500px]'>Text</h5>
                         </div>
                         <div>
                             <h1 className=''>Username:</h1>
-                            <input className='h-9 sm:w-[500px] w-[250px] border-2 rounded' type="text" name="" id="" />
+                            <h5 className='w-[200px] sm:w-[500px]'>Text</h5>
                         </div>
                         <div>
                             <h1 className=''>Email:</h1>
-                            <input className='h-9 sm:w-[500px] w-[250px] border-2 rounded' type="text" name="" id="" />
+                            <h5 className='w-[200px] sm:w-[500px]'>Text</h5>
                         </div>
                         <div>
                             <h1 className=''>Address:</h1>
-                            <input className='h-9 sm:w-[500px] w-[250px] border-2 rounded' type="text" name="" id="" />
+                            <h5 className='w-[200px] sm:w-[500px]'>Text</h5>
                         </div>
                         <div>
                             <h1 className=''>Amount:</h1>
-                            <input className='h-9 sm:w-[500px] w-[250px] border-2 rounded' type="text" name="" id="" />
+                            <h5 className='w-[200px] sm:w-[500px]'>Text</h5>
                         </div>
-                        <div className='flex md:flex-row flex-col justify-center'>
-                            <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Revert changes</button>
-                            <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Save changes</button>
-                        </div>                </div>
+                    </div>
 
 
                     <Link href="/orders">
