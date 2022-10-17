a81046b3b22bd37db9ee347602d814b934f33f7d
drensokoli <79976300+drensokoli@users.noreply.github.com>
2022-10-17 15:48:01 +0200

    User info design changes

--- elefanti60_front/pages/userinfo.js
+++ elefanti60_front/pages/userinfo.js
@@ -7,16 +7,16 @@ import { useState, useEffect } from 'react'
 
 export default function UserInfoScreen() {
 
-    const[isLoading, setIsLoading] = useState(true);
-    const[userData, setUserData] = useState(null);
-     const router = useRouter();
-     const logout = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [userData, setUserData] = useState(null);
+    const router = useRouter();
+    const logout = () => {
         localStorage.setItem("id", null);
         router.push("/")
-     }
+    }
 
-     useEffect(() => {
-        async function fetchUserData(){
+    useEffect(() => {
+        async function fetchUserData() {
             const userId = localStorage.getItem('id');
             const https = require("https");
             const agent = new https.Agent({ rejectUnauthorized: false })
@@ -28,48 +28,46 @@ export default function UserInfoScreen() {
         fetchUserData()
     }, [])
 
-    if(isLoading){
+    if (isLoading) {
         return (
-            <Layout title="User Info">    
+            <Layout title="User Info">
             </Layout >
         )
     }
 
-    console.log(userData,"datata")
     return (
         <Layout title="User Info">
-            <div className='flex flex-col justify-center items-center gap-4 h-screen mb-5'>
-                <div className=" flex flex-col w-fit gap-3 h-fit">
-                    <div className=" flex flex-col w-fit border-2 p-10 gap-3 h-fit">
+            <div className='flex flex-col items-center gap-4 h-fit m-20'>
+                <div className=" flex flex-col w-fit gap-3">
+                    <div className=" flex flex-col w-fit border-2 p-10 gap-3">
                         <div className='flex flex-row justify-between'>
-                            <h1 className='text-3xl text-left text-blue-500 '>Informata të përdoruesit</h1>
+                            <h1 className='text-3xl text-left text-blue-500 '>Informatat e përdoruesit</h1>
+                        </div>
+                        <div>
+                            <h1 className='text-xs text-gray-500'>Emri:</h1>
+                            <h5 className='w-[200px] sm:w-[500px]'>{userData.name}</h5>
+                        </div>
+                        <div>
+                            <h1 className='text-xs text-gray-500'>Mbiemri:</h1>
+                            <h5 className='w-[200px] sm:w-[500px]t-lg'>{userData.lastName}</h5>
+                        </div>
+                        <div>
+                            <h1 className='text-xs text-gray-500'>Emri i përdoruesit:</h1>
+                            <h5 className='w-[200px] sm:w-[500px]t-lg'>{userData.username}</h5>
+                        </div>
+                        <div>
+                            <h1 className='text-xs text-gray-500'>Adresa juaj elektronike:</h1>
+                            <h5 className='w-[200px] sm:w-[500px] bog'>{userData.email} </h5>
+                        </div>
+                        <div>
+                            <h1 className='text-xs text-gray-500'>Adresa:</h1>
+                            <h5 className='w-[200px] sm:w-[500px] -lg'>{userData.address}</h5>
+                        </div>
+                        <div>
+                            <h1 className='text-xs text-gray-500'>Sasia:</h1>
+                            <h5 className='w-[200px] sm:w-[500px] blg'> ${userData.amount}</h5>
                         </div>
 
-                        <div>
-                            <h1 className=''>Emri:</h1>
-                        <h5 className='w-[200px] sm:w-[500px]'>{userData.name}</h5>
-                        </div>
-                        <div>
-                            <h1 className=''>Mbiemri:</h1>
-                        <h5  className='w-[200px] sm:w-[500px]t-lg'>{userData.lastName}</h5>
-                        </div>
-                        <div>
-                            <h1 className=''>Emri i përdoruesit:</h1>
-                        <h5  className='w-[200px] sm:w-[500px]t-lg'>{userData.username}</h5>
-                        </div>
-                        <div>
-                            <h1 className=''>Adresa juaj elektronike:</h1>
-                        <h5 className='w-[200px] sm:w-[500px] bog'>{userData.email} </h5>
-                        </div>
-                        <div>
-                            <h1 className=''>Adresa:</h1>
-                        <h5  className='w-[200px] sm:w-[500px] -lg'>{userData.address}</h5>
-                        </div>
-                        <div>
-                            <h1 className=''>Sasia:</h1>
-                        <h5 className='w-[200px] sm:w-[500px] blg'> ${userData.amount}</h5>
-                        </div>
-                        
                     </div>
 
                 </div>
