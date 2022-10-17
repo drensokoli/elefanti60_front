29cc3d35341c35f75b90efa5bf5cfe0ae1a642fc
drensokoli <79976300+drensokoli@users.noreply.github.com>
2022-10-17 10:40:44 +0200

    Added delete button on cart items

--- elefanti60_front/components/CartItem.jsx
+++ elefanti60_front/components/CartItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { UilTimesCircle } from '@iconscout/react-unicons'
 
 const CartItem = ({ title, desc, id, productId, price, image, quantity, total, stock }) => {
 
@@ -15,31 +16,39 @@ const CartItem = ({ title, desc, id, productId, price, image, quantity, total, s
 
     return (
         <>
-            <div class="flex flex-col sm:flex-row h-fit lg:w-[1100px] lg:gap-30 bg-white rounded-lg border border-gray-200 shadow-md justify-around pb-5">
-                <div className='flex flex-col items-center justify-center p-10 justify-items-center'>
-                    <Link href={`/product/${productId}`}>
-                        <a className="h-40">
-                            <img src={image} alt="" />
-                        </a>
-                    </Link>
-                </div>
-                <div className='flex flex-col mt-3 justify-center p-5 gap-2'>
-                    <Link href={`/product/${id}`}>
-                        <div className="flex flex-row justify-between">
-                            <h5 class=" text-xl font-bold tracking-tight text-gray-900 ">{title}</h5>
-                            <p className=" text-xl font-mediium tracking-tight text-gray-700">${price}</p>
-                        </div>
-
-                    </Link>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-500">{desc}</p>
-                    <div className="flex flex-row justify-between items-end gap-20">
-                        <div className="flex flex-col justify-center items-start">
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-500">{inStock}</p>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-500">Quantity: {quantity}</p>
-                        </div>
-                        <h5 class=" text-xl font-bold tracking-tight text-gray-900 ">Total: ${total}</h5>
+            <div>
+                <div className="relative">
+                    <div className="absolute right-1 top-1">
+                    <UilTimesCircle />
                     </div>
+                </div>
 
+                <div class="flex flex-col sm:flex-row h-fit lg:w-[1100px] lg:gap-30 bg-white rounded-lg border border-gray-200 shadow-md justify-around pb-5">
+                    <div className='flex flex-col items-center justify-center p-10 justify-items-center'>
+                        <Link href={`/product/${productId}`}>
+                            <a className="h-40">
+                                <img src={image} alt="" />
+                            </a>
+                        </Link>
+                    </div>
+                    <div className='flex flex-col mt-3 justify-center p-5 gap-2'>
+                        <Link href={`/product/${id}`}>
+                            <div className="flex flex-row justify-between">
+                                <h5 class=" text-xl font-bold tracking-tight text-gray-900 ">{title}</h5>
+                                <p className=" text-xl font-mediium tracking-tight text-gray-700">${price}</p>
+                            </div>
+
+                        </Link>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-500">{desc}</p>
+                        <div className="flex flex-row justify-between items-end gap-20">
+                            <div className="flex flex-col justify-center items-start">
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-500">{inStock}</p>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-500">Quantity: {quantity}</p>
+                            </div>
+                            <h5 class=" text-xl font-bold tracking-tight text-gray-900 ">Total: ${total}</h5>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </>
